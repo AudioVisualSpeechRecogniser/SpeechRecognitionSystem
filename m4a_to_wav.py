d30055db5a82870db0daa7dbdd52c915571a5b7a
Ben Cooper <ben.f.cooper@gmail.com>
2021-10-29 12:31:43 +0100

    Added training data

--- m4a_to_wav.py
+++ m4a_to_wav.py
@@ -4,5 +4,5 @@ for root, dirs, files in os.walk(".", topdown=False):
       filename, file_extension = os.path.splitext(os.path.join(root, name))
       print(file_extension)
       if file_extension == ".m4a":
-          print(f"ffmpeg -i {filename + file_extension} -vn {filename}.wav")
-          os.system(f"ffmpeg -i \"{filename + file_extension}\" -vn \"{filename}.wav\"")
+          print(f"Converting... ffmpeg -i {filename + file_extension} -vn {filename}.wav")
+          os.system(f"ffmpeg -i \"{filename + file_extension}\" -vn \"{filename}Session2.wav\"")
