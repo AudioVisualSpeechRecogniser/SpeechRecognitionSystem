25b592f1b78259d143e66b07b1efd46f531669e0
Ben Cooper <ben.f.cooper@gmail.com>
2021-10-29 12:12:47 +0100

    Added conersion script

---
+++ m4a_to_wav.py
@@ -0,0 +1,8 @@
+import os
+for root, dirs, files in os.walk(".", topdown=False):
+   for name in files:
+      filename, file_extension = os.path.splitext(os.path.join(root, name))
+      print(file_extension)
+      if file_extension == ".m4a":
+          print(f"ffmpeg -i {filename + file_extension} -vn {filename}.wav")
+          os.system(f"ffmpeg -i \"{filename + file_extension}\" -vn \"{filename}.wav\"")
