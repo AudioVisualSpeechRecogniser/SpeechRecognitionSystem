1cb5f522e43982268a1fe0701143ff2b2d5c0de8
Tendai Phikiso <tphikiso@gmail.com>
2021-11-08 18:52:01 +0000

    Fixed speech capture and added test audio

--- main.m
+++ main.m
@@ -1,16 +1,16 @@
 numberOfFiles = 1; % Number of files in the train/test folder
-fileNameStart = "reversed-"; % Beginning of the filename without the number e.g. "Take-" for "Take-1" et.c
+fileNameStart = "Speech-"; % Beginning of the filename without the number e.g. "Take-" for "Take-1" et.c
 filterbankChannels = 30; % Number of filterbank channels to use
 
 for i=1:numberOfFiles
     currentFile = fileNameStart + i;
     numberOfChannels = filterbankChannels;
 
-    input = "TrainingData\Training-Audio\" + currentFile + ".wav";
+    input = "TrainingData\Testing-Audio\" + currentFile + ".wav";
 
     disp(input)
 
-    output = "MFCCs\train\" + currentFile + ".mfc";
+    output = "MFCCs\test\" + currentFile + ".mfc";
 
     [sample,fs] = audioread(input);
 
--- speech_capture.m
+++ speech_capture.m
@@ -1,25 +1,21 @@
-for fileNumber = 1:5
-    fs = 16000;
-    r = audiorecorder(fs, 16,1);
-    disp("Start speaking");
-    record(r);
-    for i = 5:-1:1 %reverse counting 
-        pause(1); 
-        disp(i);
-    end 
+function[] = speech_capture()
+    for fileNumber = 1:1 % Number of recordings to create 
+        fs = 16000; % Sampling frequency of 16KHz
+        r = audiorecorder(fs, 16,1);
+        disp("Start speaking");
+        record(r); % Record Audio
+        
+        pause; 
+    
+        disp("Saving Audio...");
+    
+        stop(r); 
+    
+        x = getaudiodata(r, 'double');
+    
+        xNorm = 0.99 * x / max(abs(x)); % Normalise audio data 
+        audiowrite("Speech-" + fileNumber + ".wav", xNorm, 16000); % Saving audio data
 
-    disp("Stop speaking");
-
-    stop(r);
-
-    x = getaudiodata(r, 'double');
-
-    t = [1/fs: 1/fs: length(x)/fs ];
-
-    xNorm = 0.99 * x / max(abs(x));
-    audiowrite("speech" + fileNumber + ".wav", xNorm, 16000);
-
-    pause(2);
-
-    clear;
+        disp("Audio Saved :)");
+    end
 end
