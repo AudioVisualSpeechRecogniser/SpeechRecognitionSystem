194001ca919369bbb2f6ab668d006daca037028b
ccg18fdu <ccg18fdu@uea.ac.uk>
2021-11-08 19:30:01 +0000

    testing update

--- writeToFile.m
+++ writeToFile.m
@@ -10,15 +10,12 @@ sampSize = 4 * columns;
 parmKind = 6; %MFCC  
 data = featureVectors;
 
-
-% Write the header information% 
 fwrite(fid, nSamples, 'int32');   % number of vectors in file (4 byteint)
 fwrite(fid, sampPeriod, 'int32'); % sample period in 100ns units (4 byte int)
 fwrite(fid, sampSize, 'int16');   % number of bytes per sample (2 byte int)
 fwrite(fid, parmKind, 'int16');   % code for the sample kind (2 byte int)
 
-% Write the data: one coefficient at a time:
-for i = 1:nSamples
+for i = 1:nSamples % Write data to the file
     for j = 1:columns
 		fwrite(fid, data(i, j), 'float32'); 
     end
@@ -27,4 +24,3 @@ end
 disp("Written : " + filename);
 
 end
-
