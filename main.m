ff831178cffce93993920d54516528bfa1ccfd2a
wtn18vcu <wtn18vcu@CMPLAB1-09.local>
2021-11-08 20:42:51 +0000

    Merge branch 'master' of https://github.com/AudioVisualSpeechRecogniser/SpeechRecognitionSystem into master
    # Conflicts:
    #	main.m
    #	speech_capture.asv

--- main.m
+++ main.m
@@ -1,16 +1,21 @@
 numberOfFiles = 1; % Number of files in the train/test folder
 fileNameStart = "Speech-"; % Beginning of the filename without the number e.g. "Take-" for "Take-1" et.c
 filterbankChannels = 30; % Number of filterbank channels to use
+testOrTrain = "test";
 
 for i=1:numberOfFiles
     currentFile = fileNameStart + i;
     numberOfChannels = filterbankChannels;
 
-    input = "TrainingData\Testing-Audio\" + currentFile + ".wav";
+    input = "Data\" + testOrTrain + "\" + currentFile + ".wav";
 
     disp(input)
 
-    output = "MFCCs\test\" + currentFile + ".mfc";
+    output = "MFCCs\" + testOrTrain + "\" + currentFile + ".mfc";
+    
+    fid = fopen("lists/" + testOrTrain + "List.txt", "w");
+
+    fprintf(fid, "MFCCs/" + testOrTrain + "/" + currentFile + ".mfc");
 
     [sample,fs] = audioread(input);
 
